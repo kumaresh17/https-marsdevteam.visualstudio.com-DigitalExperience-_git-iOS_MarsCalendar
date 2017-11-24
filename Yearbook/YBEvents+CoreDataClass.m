d34c5a1a10d14c55f11818bff25ed4d0b032b656
kumaresh17 <Kumaresh@kumareshs-mbp.mars-ad.net>
2017-11-24 11:44:22 +0100

    Final Mars Profile App on intune

--- Yearbook/YBEvents+CoreDataClass.m
+++ Yearbook/YBEvents+CoreDataClass.m
@@ -5,7 +5,7 @@
 //  Created by Urmil Setia on 21/11/2016.
 //  Copyright © 2016 Mars IS. All rights reserved.
 //  This file was automatically generated and should not be edited.
-//
+// Final profile Code
 
 #import "YBEvents+CoreDataClass.h"
 #import "YBCalendarCategory+CoreDataClass.h"
